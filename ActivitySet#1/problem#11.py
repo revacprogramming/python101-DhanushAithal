3a20fe45f84b2f46f27c98018c729ffa3283a504
DhanushAithal <dhanushAithal@gmail.com>
2022-06-07 19:51:18 +0530

    P11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,8 @@
-# Tuples
-
-filename =   "dataset/mbox-short.txt"
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names :
+    if name not in counts: 
+       counts[name] = 1
+    else :
+        counts[name] = counts[name] + 1
+print(counts)
