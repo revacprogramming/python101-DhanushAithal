3c2248aa4e169cdcd30ad5eef6f8d428de09bc44
DhanushAithal <dhanushAithal@gmail.com>
2022-06-08 22:58:08 +0530

    p11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,8 +1,18 @@
-counts = dict()
-names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
-for name in names :
-    if name not in counts: 
-       counts[name] = 1
-    else :
-        counts[name] = counts[name] + 1
-print(counts)
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+dic = dict()
+for line in handle:
+    word_split = line.split()
+if "From" in word_split:
+    email = word_split[1]
+dic[email] = dic.get(email,0) + 1
+holder =0
+key_result =None
+value_result =None
+for key,value in dic.items():
+    if value > holder:
+        holder =value
+key_result =key
+value_result =value
+print(key_result , value_result)
