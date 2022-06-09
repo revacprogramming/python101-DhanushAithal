7f9865e49a3b0992ffe7e35764cbc4c27c0375b5
DhanushAithal <dhanushAithal@gmail.com>
2022-06-09 22:21:02 +0530

    p11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,18 +1,17 @@
+# Dictionaries
+
+filename = "dataset/mbox-short.txt"
+
+d=dict()
 name = input("Enter file:")
-if len(name) < 1 : name = "mbox-short.txt"
+if len(name) < 1:
+    name = "mbox-short.txt"
 handle = open(name)
-dic = dict()
-for line in handle:
-    word_split = line.split()
-if "From" in word_split:
-    email = word_split[1]
-dic[email] = dic.get(email,0) + 1
-holder =0
-key_result =None
-value_result =None
-for key,value in dic.items():
-    if value > holder:
-        holder =value
-key_result =key
-value_result =value
-print(key_result , value_result)
+for i in handle:
+    if i.startswith("From "):
+        emails=i.split()
+        email=emails[1]
+        d[email]=d.get(email,0)+1
+    else:
+        continue
+print(email,d[email])
