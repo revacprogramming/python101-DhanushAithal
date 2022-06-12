d9b20f177af3ee4f2318aea4b562eda7729bf31d
DhanushAithal <dhanushAithal@gmail.com>
2022-06-12 22:47:25 +0530

    p10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -5,4 +5,4 @@ for l in file:
         adr = l.split(" ")[1]
         print(adr.rstrip())
         count += 1
-print("There were", count, "lines in the file with From as the first word")
+print("There were ", count, "lines in the file with From as the first word")
