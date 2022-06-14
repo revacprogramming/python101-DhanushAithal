51b219e3958deec344e5a4974e2f877cb5cb5d5f
DhanushAithal <dhanushAithal@gmail.com>
2022-06-14 23:45:09 +0530

    p12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,12 @@
-# Regular Expressions
-# https: //www.py4e.com/lessons/regex
+import re
+sum=0
+file_name=input("Enter the file name to open:")
+file_open=open(file_name)
+for line in file_open:
+  line=line.strip()
+  find_num=re.findall('[0-9]+',line)
+  #find_num=re.findall('\d+',line)
+  for num in find_num:
+    y=float(num)
+    sum+=y
+print(sum)
