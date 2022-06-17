432a5bbc9a495b740087e21d0e33591dd596d667
DhanushAithal <dhanushAithal@gmail.com>
2022-06-17 19:18:58 +0530

    p13

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,18 @@
-# Network Programming
-#https://www.py4e.com/lessons/network
+# Tuples
+filename=input("Enter the filename:")
+fopen=open(filename)
+di_ct=dict()
+lst=list()
+count=0
+for line in fopen:
+  if line.startswith("From:"):
+    continue
+  if line.startswith("From"):
+    line=line.split()
+    time=line[5]
+    hr=time[:2]#[0:2]
+    di_ct[hr]=di_ct.get(hr,0)+1
+lst=list(di_ct.items())
+lst.sort()
+for i in lst:
+  print(i[0],i[1])
---
+++ ActivitySet#1/problem#18.py
@@ -0,0 +1,9 @@
+import re
+count = 0
+hand = open("regex_sum_1548641.txt.opdownload")
+lines = hand.read()
+match = re.findall('[0-9]+',lines)
+for i in match:
+	number = int(i)
+	count = count + number
+print(count)
