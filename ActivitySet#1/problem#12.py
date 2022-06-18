9b35579926a6d1619d85b17e11d4d944c7b00eca
DhanushAithal <dhanushAithal@gmail.com>
2022-06-18 23:15:45 +0530

    p13

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -4,7 +4,7 @@ file_name=input("Enter the file name to open:")
 file_open= open(file_name)
 for line in file_open:
   line=line.strip()
-  find_num=re.findall('[0-9]+',line)
+  find_num= re.findall('[0-9]+',line)
   #find_num=re.findall('\d+',line)
   for num in find_num:
     y=float(num)
