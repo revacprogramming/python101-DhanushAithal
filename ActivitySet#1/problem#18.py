411fcdc4519a31a034b145574742d8939e4295db
DhanushAithal <dhanushAithal@gmail.com>
2022-07-05 22:14:56 +0530

    p18

--- ActivitySet#1/problem#18.py
+++ ActivitySet#1/problem#18.py
@@ -2,7 +2,7 @@ import re
 count = 0
 hand = open("regex_sum_1548641.txt.opdownload")
 lines = hand.read()
-match = re.findall('[0-9]+',lines)
+match =  re.findall('[0-9]+',lines)
 for i in match:
 	number = int(i)
 	count = count + number
