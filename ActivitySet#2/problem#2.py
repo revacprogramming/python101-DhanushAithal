5c5decf6be899a0cb96360540ee364e896b5b682
DhanushAithal <dhanushAithal@gmail.com>
2022-07-10 11:02:10 +0530

    p2

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,18 +1,21 @@
 
+def input_two_numbers():
+  n=input("Enter a Number to add: ")
+  return n
+  
 def add(a, b):
-    pass  # ...
-
+    return a+b
 
 def output(a, b, sum):
-    pass  # ...
-
+    print("Addition of %d + %d is :"%(a,b),sum)
 
 def main():
-    a, b = input_two_numbers()
+    a= int(input_two_numbers())
+    b=int(input_two_numbers())
     sum = add(a, b)
-
     output(a, b, sum)
 
 
+
 if __name__ == '__main__':
-    main()
+  main()
