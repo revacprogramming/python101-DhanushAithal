63998da829d5103e54ea244a28b00e5bad630613
DhanushAithal <dhanushAithal@gmail.com>
2022-07-11 17:40:47 +0530

    p3

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,19 +1,27 @@
+def get_str():
+   """get string input"""
+   string=''
+   string=input("Enter the string:")
+   return string
 
 
-def get_cs():
-    """get string input"""
-
-
-def cs_to_lot(cs):
+def str_to_lst(cs):
     """convert connected string to list of strings"""
-
+    split=cs.split(';')
+    lst=list()
+    #if '' in split:
+      #split.remove('')
+    for word in split:
+      wrd=word.split('=')
+      t=tuple(wrd)
+      lst.append(t)
+    return lst
 
 def main():
-    cs = get_cs()
-
-    lot = cs_to_lot(cs)
-    print(lot)
+   name=get_str()
+   list=str_to_lst(name)
+   print(list)
 
 
 if __name__ == '__main__':
-    main()
+ main()
