42de05c1fe99c15ceff6710f64f2f938899ff39b
DhanushAithal <dhanushAithal@gmail.com>
2022-07-12 16:14:24 +0530

    p4

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -2,25 +2,41 @@
 
 def get_cs():
     """get string input"""
+    string=str(input("Enter the string :"))
+    return string
 
 
-def cs_to_lot(cs):
+def str_to_lst(cs):
     """convert connected string to list of strings"""
+    l=[]
+    spt=cs.split(';')
+    for word in spt:
+        word=word.split('=')
+        t=tuple(word)
+        l.append(t)
+    return l
 
 
-def lot_to_cs(lot):
+def lst_to_str(lot):
     """convert list of strings to connected string"""
+    str=""
+    l=[]
+    for wrd in lot:
+        l.append(wrd)
+        
+    for i in l:
+            str+=i[0]+'='+i[1]+';'
+            
+    return str
 
-
+print(f"present Dictionary/-_name__=={__name__}")
 def main():
-    cs=get_cs()
-
-    lot=cs_to_lot(cs)  # convert connect string to list of tuples
-    print(lot)
-
-    cs=lot_to_cs(lot)  # convert list of strings to connect string
-    print(cs)
-
+    name=get_cs()
+    list=str_to_lst(name)
+    string=lst_to_str(list)
+    print(list)
+    print(string)
+    #system=s;database=d;username=u;passwd=psystem=s;database=d;username=u;passwd=pprint(string)
 
 if __name__ == '__main__':
     main()
