1bb1867f9f675586b2b082835c7da8861e76ba39
DhanushAithal <dhanushAithal@gmail.com>
2022-07-17 20:52:23 +0530

    p6

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,11 +1,29 @@
 
 
-class Menu:
-    """fill in class definition here"""
+print(" Menu List ".center(40,'*'))
+print('_'*34)
+print('Item'+'->'+'Rate')
+class Menu(dict):
+  """fill in class definition here"""
+  def __init__(self):
+    self=dict()
+    '''  self.item=item
+    self.rate=rate'''
+  def add(self,item,rate):
+    self[item]=rate
+    
+  def show(self):
+    for i,j in self.items():
+     print(i,'->',j)
 
-
-m =Menu()  # Menu is a class
-m.add("idly", 10)
-m.add("vada". 20)
+m=Menu()
+m.add("Idly",20)
+m.add("Vada",10)
+m.add("Dosa",25)
+m.add("Poori",30)
 
 m.show()
+'''poori.show()
+idly.show()
+vada.show()
+dosa.show()
