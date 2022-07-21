55f7d6d57c78e030845f9bd8a5940a2e3a4f2a2e
DhanushAithal <dhanushAithal@gmail.com>
2022-07-22 01:48:40 +0530

    p8

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -1,13 +1,16 @@
 
 
-class Menu:
-    """fill in class definition here"""
-    def __init__(self,item,rate):
-        self.item=item
-        self.rate=rate
-    def __add(self):
-m = Menu()
-m["idly"] = 10
-m["vada"] = 20
+class Menu(dict):
+  def __init__(self):
+   pass
+  def dict(self,item,rate):
+    self[item]=rate
+    
+menu=Menu()
+menu['Idly']=20
+menu['Dosa']=25
+menu['Vada']=10
 
-print(m)
+print(menu)
+for k,v in menu.items():
+  print(k,v)
